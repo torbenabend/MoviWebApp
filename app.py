e6a41be5cc03cb86cd0b5b5e2c84309b7fdcc08f
Torben Abend <torben.abend@gmail.com>
2026-03-30 15:31:21 +0200

    Add initial version of flask web app

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask
+import os
+from data_manager import DataManager
+from models import db, Movie
+
+
+app = Flask(__name__)
+
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{os.path.join(basedir, 'data/movies.db')}"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)  # Link the database and the app. This is the reason you need to import db from models
+
+data_manager = DataManager() # Create an object of your DataManager class
+
+
+@app.route('/')
+def home():
+    return "Welcome to MoviWeb App!"
+
+
+if __name__ == '__main__':
+  #with app.app_context():
+  #  db.create_all()
+
+  app.run()
